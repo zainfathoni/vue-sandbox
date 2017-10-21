36388ebceb0468fa68a968fb16c5777bc6d8c6a0
Zain Fathoni <zain.fathoni@gmail.com>
2017-10-22 00:46:37 +0700

    step: 29. Finalizing Character Behavior

--- applications/starbase/src/components/Character.vue
+++ applications/starbase/src/components/Character.vue
@@ -1,29 +1,34 @@
 <template>
-  <div>
+  <div @click="switchCharacter">
     {{character}}
   </div>
 </template>
 
 <script>
 export default {
+  props: ['id'],
   data() {
     return {
       character: {}
     }
   },
   methods: {
-    fetchCharacter() {
+    fetchCharacter(id) {
       // Use this Chrome Extension to solve CORS problem
       // https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en
-      fetch('https://swapi.co/api/people/1', {
+      fetch(`https://swapi.co/api/people/${id}`, {
         method: 'GET'
       })
         .then(response => response.json())
         .then(json => (this.character = json))
+    },
+    switchCharacter() {
+      let random_id = Math.floor(Math.random() * 83) + 1
+      this.fetchCharacter(random_id)
     }
   },
   created() {
-    this.fetchCharacter()
+    this.fetchCharacter(this.id)
   }
 }
 </script>
