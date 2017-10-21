4959d58ecb3e0b8febf20dc88da73322637e1916
Zain Fathoni <zain.fathoni@gmail.com>
2017-10-22 06:14:24 +0700

    step: 49. Building a Joke Component

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -4,6 +4,15 @@
     <button class="btn btn-primary" @click="initJokes">
       Add Ten Random Jokes
     </button>
+    <br>
+    <br>
+    <div class="col-md-12">
+      <Joke
+        v-for="(joke, index) in $store.state.jokes"
+        :joke="joke"
+        :key="index"
+      />
+    </div>
     <div>
       {{$store.state}}
     </div>
@@ -12,9 +21,13 @@
 
 <script>
 import { mapActions } from 'vuex'
+import Joke from './Joke.vue'
 
 export default {
-  methods: mapActions(['initJokes'])
+  methods: mapActions(['initJokes']),
+  components: {
+    Joke
+  }
 }
 </script>
 
