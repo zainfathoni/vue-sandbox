d3080f057d78ebe370a7e1d3b8c26cce8a50ac74
Zain Fathoni <zain.fathoni@gmail.com>
2017-10-22 06:08:05 +0700

    step: 47. Acting Upon the Store

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
-    Got jokes?
-    {{$store.state}}
+    <h3>Got jokes?</h3>
+    <button class="btn btn-primary" @click="initJokes">
+      Add Ten Random Jokes
+    </button>
+    <div>
+      {{$store.state}}
+    </div>
   </div>
 </template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  methods: mapActions(['initJokes'])
+}
+</script>
+
