805c5c214b1a1cdeaff91459d0d48b2a06eb57a3
Zain Fathoni <zain.fathoni@gmail.com>
2017-10-22 00:27:08 +0700

    step: 27. Introducing the API

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <h3>{{title}}</h3>
+    <button class="btn btn-primary" @click="fetchCharacter">
+      Test Fetch Character
+    </button>
   </div>
 </template>
 
@@ -10,6 +13,17 @@ export default {
   data() {
     return {
       title: 'Generate Your Team'
+    }
+  },
+  methods: {
+    fetchCharacter() {
+      // Use this Chrome Extension to solve CORS problem
+      // https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en
+      fetch('https://swapi.co/api/people/1', {
+        method: 'GET'
+      })
+        .then(response => response.json())
+        .then(json => console.log(json))
     }
   }
 }
