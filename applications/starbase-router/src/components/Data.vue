b3348561169581ed32b7f7670292377bd713d11d
Zain Fathoni <zain.fathoni@gmail.com>
2017-10-22 01:17:05 +0700

    step: 35. Introducing VueRouter

---
+++ applications/starbase-router/src/components/Data.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Type: {{type}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      type: this.$route.params.type
+    }
+  }
+}
+</script>
