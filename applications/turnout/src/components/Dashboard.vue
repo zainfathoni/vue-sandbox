5da738f31fa07f016ab7f99e641fd626c1bde91f
Zain Fathoni <zain.fathoni@gmail.com>
2017-10-22 18:58:22 +0700

    step: 60. Signing Out

--- applications/turnout/src/components/Dashboard.vue
+++ applications/turnout/src/components/Dashboard.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
-    Dashboard Component
+    <h3>Events Dashboard</h3>
+    <button
+      class="btn btn-danger btn-sm signout-btn"
+      @click="signOut"
+    >
+      Sign Out
+    </button>
 
     {{$store.state}}
   </div>
 </template>
+
+<script>
+import { firebaseApp } from '../firebaseApp'
+
+export default {
+  methods: {
+    signOut() {
+      this.$store.dispatch('signOut')
+      firebaseApp.auth().signOut()
+    }
+  }
+}
+</script>
